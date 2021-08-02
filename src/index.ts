ace2af55bdde478b219f15a08dd1dd13080aa3e7
Hetal2425 <41511579+Hetal2425@users.noreply.github.com>
2021-08-02 18:10:02 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-import * as express from "express";
+var http = require('http');
+const url = require('url');
 
-const app = express();
-app.use(express.json());
-const methodRouter = require('./methodRouter.route');
+const {
+    PostData,
+    GetData,
+    DeleteData,
+    PatchData
+} = require('./methodRouter.controller');
 
-// register routes
-app.use('/', methodRouter);
 
-//port
-app.listen(3000);
+const server = http.createServer(async (req, res) => {
+
+    if (req.url.match(/\/user?/) && req.method === "GET") {
+        GetData(req, res)
+    }
+    else if (req.url === "/user" && req.method === "GET") {
+        GetData(req, res)
+    }
+    else if (req.url === "/user" && req.method === "POST") {
+        PostData(req, res)
+    }
+    else if(req.url.match(/\/user?/) && req.method === "PATCH") {
+        PatchData(req, res)
+    }
+    else if(req.url.match(/\/user?/) && req.method === "DELETE") {
+        DeleteData(req, res)
+    }
+
+})
+server.listen(3000, () => {
+    console.log(`server started on port: 3000`);
+});
